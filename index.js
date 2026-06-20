433486dcc05acbb31cfa116674361c25e6e35779
Karan Jadhav <karanjadhav041006@gmail.com>
2026-06-20 11:20:41 +0530

    Bot stores the images

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 require('dotenv').config();
+const fs = require("fs");
+const path = require("path");
+const axios = require("axios");
+const{ connectToMongo} = require('./connect')
+
+const mongoose = require('mongoose');
+connectToMongo ('mongodb://127.0.0.1:27017/short-url')
+.then(() => console.log("Mongo Connected"))
+.catch( (err) => console.log(" MongoDB",err))
+
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
@@ -24,6 +34,8 @@ async function shortenWithTinyURL(longUrl) {
 client.on("messageCreate", async (message) => {  // ← must be async
   if (message.author.bot) return;
 
+
+
   if (message.content.startsWith("create")) {
     const longUrl = message.content.split("create")[1]?.trim();
 
@@ -41,6 +53,41 @@ client.on("messageCreate", async (message) => {  // ← must be async
     }
   }
 
+  // if(message.content.startsWith("photo")) {
+
+        if (message.attachments.size > 0) {
+    const attachment = message.attachments.first();
+
+    const imageUrl = attachment.url;
+    const fileName = attachment.name;
+
+    const filePath = path.join(__dirname, "images", fileName);
+
+    const response = await axios({
+      method: "GET",
+      url: imageUrl,
+      responseType: "stream",
+    });
+
+    const writer = fs.createWriteStream(filePath);
+
+    response.data.pipe(writer);
+
+    writer.on("finish", () => {
+      console.log(`${fileName} saved successfully`);
+    });
+
+    writer.on("error", (err) => {
+      console.log(err);
+    });
+
+
+
+    return message.reply({ content: "Image is uploaded to server" });
+  }
+      
+  // }
+
   // Only runs if no command matched
   message.reply({ content: "Its the bot" });
 });
@@ -49,5 +96,7 @@ client.on("interactionCreate", (interaction) => {
   console.log(interaction);
   interaction.reply("Pong!!");
 });
+
+
 
 client.login(process.env.DISCORD_KEY);
